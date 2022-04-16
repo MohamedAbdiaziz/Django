ffc0f4db07e1c393a406911843d7172beecfbe72
mohamed <86708771+MohamedAbdiaziz@users.noreply.github.com>
2022-04-16 04:48:40 +0300

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("blog.urls")),
-    path('accounts/',include('django.contrib.auth.urls'))
+    path('accounts/',include('django.contrib.auth.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
