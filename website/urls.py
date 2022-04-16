c69abf9d5fb3819730450092a1c4948e2591ef7b
mohamed <86708771+MohamedAbdiaziz@users.noreply.github.com>
2022-04-16 05:48:01 +0300

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -17,7 +17,7 @@ import django
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.static import server
+from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
