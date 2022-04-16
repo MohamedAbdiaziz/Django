6cc1b1ab936f367fd68f5e82409e26c9a3095cf0
mohamed <86708771+MohamedAbdiaziz@users.noreply.github.com>
2022-04-16 05:35:45 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from multiprocessing import context
 from unicodedata import category
 from django.shortcuts import get_object_or_404, redirect, render
-from requests import post
 from blog.models import Category, Post
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
